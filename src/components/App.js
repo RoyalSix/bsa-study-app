c734b5c186b85d225fdd6963fdf91f5facac4fe0
RoyalSix <jay.a.scottjr@biola.edu>
2020-12-29 09:59:23 -0800

    created navigation; app setup

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import NavigationBar from './NavigationBar';
+import { Container } from './styled';
+import '../style.css';
+
+function App() {
+  return (
+    <Container>
+      <NavigationBar />
+    </Container>
+  );
+}
+
+export default App;
